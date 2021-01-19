aec8ea38d607e6ffb7404326c5aabf6482a85f93
NelisW1 <niels.worrell@gmail.com>
2021-01-19 17:38:31 +0100

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -2,16 +2,18 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
 
 import os
 
-# should be passed through environment variables
-username = os.environ.get('EMAIL_USER')
-password = os.environ.get('EMAIL_PW')
 
-def send_mail(text='Email Body', subject='Hello World', from_email='',
-              to_emails=None, html_file_path=None, html=None, html_inserts=None, image_file_paths=None):
-    assert isinstance(to_emails, list)
+def send_mail(text='Email Body', subject='Hello World', from_email='', to_emails=None, host=None, user_mailserver='',
+              pw_mailserver='', html_file_path=None, html=None, html_inserts=None, image_file_paths=None,
+              att_file_paths=None):
+    assert to_emails is not None, "to_emails cannot be None."
+    assert isinstance(to_emails, list), "To emails is not a list."
+    assert host is not None, "Host cannot be None."
+    assert len(from_email) > 0, 'From email cannot be empty'
 
 
     # EMAIL OBJECT
@@ -30,7 +32,11 @@ def send_mail(text='Email Body', subject='Hello World', from_email='',
                 html = f.read()
 
             if html_inserts is not None:
-                html = html.format(**html_inserts)
+                text_parts = html.split("</style>")
+                part_1 = text_parts[0]
+                part_2 = text_parts[1].format(**html_inserts)
+                html = part_1 + "\n \t</style> \n" + part_2
+
             html_part = MIMEText(html, 'html')
             msg.attach(html_part)
         else:
@@ -40,7 +46,11 @@ def send_mail(text='Email Body', subject='Hello World', from_email='',
             if html_inserts is not None:
                 if not isinstance(html_inserts, dict):
                     raise Exception("html_inserts is not a dictionary")
-                html = html.format(**html_inserts)
+
+                text_parts = html.split("</style>")
+                part_1 = text_parts[0]
+                part_2 = text_parts[1].format(**html_inserts)
+                html = part_1 + "</style>" + part_2
 
             html_part = MIMEText(html, 'html')
             msg.attach(html_part)
@@ -70,13 +80,17 @@ def send_mail(text='Email Body', subject='Hello World', from_email='',
             raise Exception("html_inserts has to be a dictionary.")
 
     # attach image - not embedded
-    elif image_file_paths is not None:
+    if image_file_paths is not None:
         if isinstance(image_file_paths, list):
             for path in image_file_paths:
                 if os.path.isfile(path):
                     with open(path, 'rb') as f:
                         image = f.read()
                         image_name = f.name
+                        try:
+                            image_name = image_name.split("/")[-1]
+                        except:
+                            pass
                     image_part = MIMEImage(image, name=image_name)
                     msg.attach(image_part)
                 else:
@@ -86,53 +100,55 @@ def send_mail(text='Email Body', subject='Hello World', from_email='',
                 with open(image_file_paths, 'rb') as f:
                     image = f.read()
                     image_name = f.name
+                    try:
+                        image_name = image_name.split("/")[-1]
+                    except:
+                        pass
                 image_part = MIMEImage(image, name=image_name)
                 msg.attach(image_part)
         else:
-            raise Exception("Image file paths is not of the correct format. Must be a string or list.")
+            raise Exception("image_file_paths is not of the correct format. Must be a string or list.")
 
+    # attach files
+    if att_file_paths is not None:
+        if isinstance(att_file_paths, list):
+            for path in att_file_paths:
+                if os.path.isfile(path):
+                    with open(path, 'rb') as f:
+                        file = f.read()
+                        file_name = f.name
+                        try:
+                            file_name = file_name.split("/")[-1]
+                        except:
+                            pass
+                    file_part = MIMEApplication(file, name=file_name)
+                    msg.attach(file_part)
+                else:
+                    raise Exception(f"{path} is not a correct file path.")
+        elif isinstance(att_file_paths, str):
+            if os.path.isfile(att_file_paths):
+                with open(att_file_paths, 'rb') as f:
+                    file = f.read()
+                    file_name = f.name
+                    try:
+                        file_name = file_name.split("/")[-1]
+                    except:
+                        pass
+                file_part = MIMEApplication(file, name=file_name)
+                msg.attach(file_part)
+        else:
+            raise Exception("att_file_paths is not of the correct format. Must be a string or list.")
 
     msg_str = msg.as_string()
 
     # SEND EMAIL
     # login to my smtp server:
     # 1 create server, 2 configure(ehlo), 3 secure(starttls), 4 login
-    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server = smtplib.SMTP(host=host, port=587)
     server.ehlo()  # identifies with server
     server.starttls()  # creates a secure connection
     server.ehlo()  # identifies with server as encrypted connection
 
-    server.login(username, password)  # login
+    server.login(user_mailserver, pw_mailserver)  # login
     server.sendmail(from_email, to_emails, msg_str)
     server.quit()
-
-
-if __name__ == "__main__":
-    to_emails = ['something@gmail.com']
-
-    image_path = "images/facebook_logo.png"
-    first_name = "Billy"
-    last_name = "Bob"
-    cid1 = 'facebook1'
-    insert_vars = {
-                        'first_name': first_name,
-                        'last_name': last_name,
-                        'cid1': image_path
-                   }
-
-    html = """
-    <header>
-    </header>
-
-    <body>
-        <img src="cid:{cid1}" style="width: 30px;">
-        <p> My first name is {first_name} and my
-            last name is {last_name}.
-        </p>
-
-    </body>
-    """
-
-    send_mail(subject='Test email', to_emails=to_emails, from_email=username, text="see attached", html_file_path=html,
-               html_inserts=insert_vars)
-
